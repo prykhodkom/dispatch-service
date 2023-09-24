9aa57678593cd0f1c9a70754212f541115cc28e6
Michael <manchester6913@gmail.com>
2023-09-24 12:59:57 -0400

    Add login&logout pages

--- dispatch/forms.py
+++ dispatch/forms.py
@@ -3,5 +3,5 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
-from dispatch.models import Worker, Driver, Truck
+from dispatch.models import Worker, Driver
 
--- dispatch/views.py
+++ dispatch/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
@@ -7,72 +8,78 @@ from django.views import generic
 from dispatch.models import Worker, Driver, Truck, Position
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     num_workers = Worker.objects.count()
     num_drivers = Driver.objects.count()
     num_trucks = Truck.objects.count()
     num_positions = Position.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     context = {
         "num_workers": num_workers,
         "num_drivers": num_drivers,
         "num_trucks": num_trucks,
         "num_position": num_positions,
+        "num_visits": num_visits + 1,
     }
     return render(request, "dispatch/index.html", context=context)
 
 
-class PositionListView(generic.ListView):
+class PositionListView(LoginRequiredMixin, generic.ListView):
     model = Position
     paginate_by = 2
 
 
-class PositionCreateView(generic.CreateView):
+class PositionCreateView(LoginRequiredMixin, generic.CreateView):
     model = Position
     fields = "__all__"
     success_url = reverse_lazy("dispatch:position-list")
     template_name = "dispatch/position_form.html"
 
 
-class PositionUpdateView(generic.UpdateView):
+class PositionUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Position
     fields = "__all__"
     success_url = reverse_lazy("dispatch:position-list")
     template_name = "dispatch/position_form.html"
 
 
-class PositionDeleteView(generic.DeleteView):
+class PositionDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Position
     success_url = reverse_lazy("dispatch:position-list")
 
 
-class DriverListView(generic.ListView):
+class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
 
 
-class WorkerListView(generic.ListView):
+class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
 
 
-class TruckListView(generic.ListView):
+class TruckListView(LoginRequiredMixin, generic.ListView):
     model = Truck
     context_object_name = "truck-list"
 
 
-class TruckCreateView(generic.CreateView):
+class TruckCreateView(LoginRequiredMixin, generic.CreateView):
     model = Truck
     fields = "__all__"
     success_url = reverse_lazy("dispatch:truck-list")
     template_name = "dispatch/truck_form.html"
 
 
-class TruckUpdateView(generic.UpdateView):
+class TruckUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Truck
     fields = "__all__"
     success_url = reverse_lazy("dispatch:truck-list")
     template_name = "dispatch/truck_form.html"
 
 
-class TruckDeleteView(generic.DeleteView):
+class TruckDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Truck
     fields = "__all__"
     success_url = reverse_lazy("dispatch:truck-list")
